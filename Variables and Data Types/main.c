f8e35f2eb104daba6957c3f63a43b4dc3375874f
Dilukshan <65407969+dilukshann7@users.noreply.github.com>
2026-03-16 23:55:28 +0530

    Auto stash before rebase of "main"

--- Variables and Data Types/main.c
+++ Variables and Data Types/main.c
@@ -1,3 +1,14 @@
 //
 // Created by Dilukshan on 3/16/2026.
 //
+#include <stdio.h>
+
+int main() {
+    float floatNum = 2.654;
+    _Bool boolValue = 1;
+    int number = 67;
+    double doubleNum = 5632.5115151555151551545454;
+
+    printf("%f, %d, %d, %lf", floatNum, boolValue, number, doubleNum);
+    return 0;
+}
