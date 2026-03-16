05b2ec8adf51d01a3591e9d234b81e8a8b8869e2
Dilukshan <65407969+dilukshann7@users.noreply.github.com>
2026-03-17 00:11:55 +0530

    feat: Add examples for C99 bool and unsigned int types

--- Variables and Data Types/main.c
+++ Variables and Data Types/main.c
@@ -2,13 +2,18 @@
 // Created by Dilukshan on 3/16/2026.
 //
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
     float floatNum = 2.654;
+    unsigned int unsInt = -50;
     _Bool boolValue = 1;
+    bool boolValueC99 = true;
     int number = 67;
     double doubleNum = 5632.5115151555151551545454;
 
     printf("%f, %d, %d, %lf", floatNum, boolValue, number, doubleNum);
+    printf("\nBoolean: %d", boolValueC99);
+    printf("\nUnsigned Int: %d", unsInt);
     return 0;
 }
