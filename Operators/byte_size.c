16051c19bf74140ea3c34e13a0590e4097cbb7a1
Dilukshan <65407969+dilukshann7@users.noreply.github.com>
2026-04-10 04:54:11 +0530

    Add byte size program to Operators

---
+++ Operators/byte_size.c
@@ -0,0 +1,14 @@
+//
+// Created by Dilukshan on 4/10/2026.
+//
+#include <stdio.h>
+
+int main() {
+    printf("Size of Int: %llu \n", sizeof(int));
+    printf("Size of Char: %llu \n", sizeof(char));
+    printf("Size of Long: %llu \n", sizeof(long));
+    printf("Size of Long Long: %llu \n", sizeof(long long));
+    printf("Size of Double: %llu \n", sizeof(double));
+    printf("Size of Long Double: %llu", sizeof(long double));
+    return 0;
+}
